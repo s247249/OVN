4e848be4fb693f04a89d6fe2e60c29fc02a3d5a4
s247249 <s247249@studenti.polito.it>
2022-04-07 12:16:02 +0200

    Finished lab2

--- Lab2/set5.py
+++ Lab2/set5.py
@@ -37,6 +37,36 @@ def ex3():
 
 # 4. Write a Python program to convert Python dictionary objects (sort by
 #    key) to JSON data. Print the object members with indent level 4.
+def ex4():
+    py_obj = {
+        'name ': 'David ',
+        'class ': 'I',
+        'age ': 6
+    }
+    json_obj = json.dumps(py_obj, sort_keys=True, indent=4)
+    print("Python: " + str(py_obj))
+    print("JSON: " + json_obj)
+
+
+# 5. Write a Python program to create a new JSON file from an existing JSON
+#    file. Use the included json file 'states.json' and create a new json file that
+#    does not contain the 'area code' field.
+def ex5():
+    wf = open("states_no_area.json", "w")
+    with open("states.json", "r") as rf:
+        py_obj = json.load(rf)
+
+    print("Initial JSON file:")
+    for i in py_obj['states']:
+        print(str(i))
+
+    for i in py_obj['states']:
+        del i['area_codes']
+
+    json.dump(py_obj, wf)
+    print("\nOutput JSON file:")
+    for i in py_obj['states']:
+        print(str(i))
 
 
 if __name__ == '__main__':
@@ -45,8 +75,8 @@ if __name__ == '__main__':
         "1": ex1,
         "2": ex2,
         "3": ex3,
-#        "4": ex4,
-#        "5": ex5
+        "4": ex4,
+        "5": ex5
     }
 
     while a != "0":
