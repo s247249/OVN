a30604b0813040c97b20be19f531c6cd8cff4190
s247249 <s247249@studenti.polito.it>
2022-05-17 12:51:03 +0200

    Finished Lab05

---
+++ Lab05/lab05.py
@@ -0,0 +1,186 @@
+import random
+
+from network import Network
+from connection import Connection
+
+
+if __name__ == '__main__':
+
+    N_fixed = Network(10, 'nodes_fixed.json')
+    N_flex = Network(10, 'nodes_flex.json')
+    N_shannon = Network(10, 'nodes_shannon.json')
+    N_fixed.connect()
+    N_flex.connect()
+    N_shannon.connect()
+    used_paths_fixed = list()
+    used_paths_flex = list()
+    used_paths_shannon = list()
+
+    # N.draw()
+
+    nodes = N_fixed.nodes.keys()
+    node_list = list()
+
+    power = 0.001
+    for i in range(100):
+        node_list.append(random.sample(nodes, 2))
+
+    # Fixed-rate
+    # finding paths based on best latency:
+    connections = list()
+    for i in node_list:
+        connections.append(Connection(i[0], i[1], power))
+
+    used_paths_fixed = N_fixed.stream(connections)
+    path_cnt = 0
+
+    for i in connections:
+        print("\nFixed-rate connection: " + str(i.input + "->" + i.output), end='')
+
+        used_path = 'None'
+        if i.snr != 0:
+            used_path = used_paths_fixed[path_cnt]
+            path_cnt += 1
+        print("\t\t\tBest available latency path: " + used_path)
+
+        print("Latency: " + str(i.latency), end='')
+        print("\t\tSNR: " + str(i.snr))
+        print("\t\tBit rate: " + str(i.bit_rate))
+
+    # freeing lines
+    for i in N_fixed.lines.values():
+        for j in range(N_fixed.number_of_channels):
+            i.set_state(j, 1)
+
+    N_fixed.route_space.to_csv('used_paths_fixed_lat.csv')
+    N_fixed.reset_route_space()
+
+    # finding paths based on best snr:
+    connections = list()
+    for i in node_list:
+        connections.append(Connection(i[0], i[1], power))
+
+    used_paths_fixed = N_fixed.stream(connections, 'snr')
+    path_cnt = 0
+
+    for i in connections:
+        print("\nFixed-rate connection: " + str(i.input + "->" + i.output), end='')
+
+        used_path = 'None'
+        if i.snr != 0:
+            used_path = used_paths_fixed[path_cnt]
+            path_cnt += 1
+        print("\t\t\tBest available SNR path found: " + used_path)
+
+        print("Latency: " + str(i.latency), end='')
+        print("\t\tSNR: " + str(i.snr))
+        print("\t\tBit rate: " + str(i.bit_rate))
+
+    N_fixed.route_space.to_csv('used_paths_fixed_snr.csv')
+
+    # Flex-rate
+    # finding paths based on best latency:
+    connections = list()
+    for i in node_list:
+        connections.append(Connection(i[0], i[1], power))
+
+    used_paths_flex = N_flex.stream(connections)
+    path_cnt = 0
+
+    for i in connections:
+        print("\nFlex-rate connection: " + str(i.input + "->" + i.output), end='')
+
+        used_path = 'None'
+        if i.snr != 0:
+            used_path = used_paths_flex[path_cnt]
+            path_cnt += 1
+        print("\t\t\tBest available latency path: " + used_path)
+
+        print("Latency: " + str(i.latency), end='')
+        print("\t\tSNR: " + str(i.snr))
+        print("\t\tBit rate: " + str(i.bit_rate))
+
+    # freeing lines
+    for i in N_flex.lines.values():
+        for j in range(N_flex.number_of_channels):
+            i.set_state(j, 1)
+
+    N_flex.route_space.to_csv('used_paths_flex_lat.csv')
+    N_flex.reset_route_space()
+
+    # finding paths based on best snr:
+    connections = list()
+    for i in node_list:
+        connections.append(Connection(i[0], i[1], power))
+
+    used_paths_flex = N_flex.stream(connections, 'snr')
+    path_cnt = 0
+
+    for i in connections:
+        print("\nFlex-rate connection: " + str(i.input + "->" + i.output), end='')
+
+        used_path = 'None'
+        if i.snr != 0:
+            used_path = used_paths_flex[path_cnt]
+            path_cnt += 1
+        print("\t\t\tBest available SNR path found: " + used_path)
+
+        print("Latency: " + str(i.latency), end='')
+        print("\t\tSNR: " + str(i.snr))
+        print("\t\tBit rate: " + str(i.bit_rate))
+
+    N_flex.route_space.to_csv('used_paths_flex_snr.csv')
+
+    # Shannon
+    # finding paths based on best latency:
+    connections = list()
+    for i in node_list:
+        connections.append(Connection(i[0], i[1], power))
+
+    used_paths_shannon = N_shannon.stream(connections)
+    path_cnt = 0
+
+    for i in connections:
+        print("\nShannon connection: " + str(i.input + "->" + i.output), end='')
+
+        used_path = 'None'
+        if i.snr != 0:
+            used_path = used_paths_shannon[path_cnt]
+            path_cnt += 1
+        print("\t\t\tBest available latency path: " + used_path)
+
+        print("Latency: " + str(i.latency), end='')
+        print("\t\tSNR: " + str(i.snr))
+        print("\t\tBit rate: " + str(i.bit_rate))
+
+    # freeing lines
+    for i in N_shannon.lines.values():
+        for j in range(N_shannon.number_of_channels):
+            i.set_state(j, 1)
+
+    N_shannon.route_space.to_csv('used_paths_shannon_lat.csv')
+    N_shannon.reset_route_space()
+
+    # finding paths based on best snr:
+    connections = list()
+    for i in node_list:
+        connections.append(Connection(i[0], i[1], power))
+
+    used_paths_shannon = N_shannon.stream(connections, 'snr')
+    path_cnt = 0
+
+    for i in connections:
+        print("\nShannon connection: " + str(i.input + "->" + i.output), end='')
+
+        used_path = 'None'
+        if i.snr != 0:
+            used_path = used_paths_shannon[path_cnt]
+            path_cnt += 1
+        print("\t\t\tBest available SNR path found: " + used_path)
+
+        print("Latency: " + str(i.latency), end='')
+        print("\t\tSNR: " + str(i.snr))
+        print("\t\tBit rate: " + str(i.bit_rate))
+
+    N_shannon.route_space.to_csv('used_paths_shannon_snr.csv')
+    N_fixed.graph()
---
+++ Lab05/line.py
@@ -0,0 +1,137 @@
+from lab05_line_methods import Line5 as line5
+
+
+class Line:
+    def __init__(self, label, length, number_of_channels):
+        self._label = label
+        self._length = length
+        self._successive = dict()
+        self._state = list()
+
+        # 1. Include in the class Line the attribute n_amplifiers that express the
+        #    number of optical amplifiers on that line. This number has to be cal-
+        #    culated using the line length, supposing that an amplifier is necessary
+        #    every 80 km. A line includes an additional amplifier at each terminal
+        #    (the BOOSTER and the Pre-Amplifier). Moreover, include the attributes
+        #    gain and noise_figure that are fixed for each amplifier to 16 dB and 3 dB,
+        #    respectively.
+        self._n_amplifiers = 2
+        # dB
+        self._gain = 16
+        # dB
+        self._noise_figure = 3
+
+        # 3. Include in the class Line an attribute for the following physical features of
+        #    the fibers necessary to evaluate the nonlinear interference noise, supposing
+        #    that all the lines are composed of the same fiber variety:
+        #    Remember that alpha = alpha_dB/(10log10(e)) and L_eff = 1/alpha
+
+        # dB/m
+        self._alpha_dB = 0.2e-3
+        # (m*Hz^2)-1
+        self._beta_2 = 2.13e-26
+        # (m*W)^-1
+        self._gamma = 1.27e-3
+        # GHz
+        self._Rs = 32
+        # GHz
+        self._df = 50
+
+        for i in range(number_of_channels):
+            self._state.append(1)
+
+        len = int(self._length)
+        while len > 0:
+            len -= 80e3
+            if len > 0:
+                self._n_amplifiers += 1
+
+    @property
+    def label(self):
+        return self._label
+
+    @property
+    def length(self):
+        return self._length
+
+    @property
+    def successive(self):
+        return self._successive
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def n_amplifiers(self):
+        return self._n_amplifiers
+
+    @property
+    def gain(self):
+        return self._gain
+
+    @property
+    def noise_figure(self):
+        return self._noise_figure
+
+    @property
+    def alpha_dB(self):
+        return self._alpha_dB
+
+    @property
+    def beta_2(self):
+        return self._beta_2
+
+    @property
+    def gamma(self):
+        return self._gamma
+
+    @property
+    def Rs(self):
+        return self._Rs
+
+    @property
+    def df(self):
+        return self._df
+
+    @label.setter
+    def label(self, label):
+        self._label = label
+
+    @length.setter
+    def length(self, length):
+        self._length = length
+
+    def set_state(self, index, state):
+        self._state[index] = state
+
+    def set_successive(self, node):
+        self._successive[node.label] = node
+
+    def latency_generation(self):
+        return float(self._length / (3e8 * 2/3))
+
+    def noise_generation(self, signal_power):
+        return float(1e-9 * signal_power * self.length)
+
+    def propagate(self, signal):
+        latency = self.latency_generation()
+        noise = self.noise_generation(signal.signal_power)
+        signal.update_latency(latency)
+        signal.update_noise_power(noise)
+        for i in self.successive.values():
+            i.propagate(signal)
+
+    def probe(self, signal):
+        latency = self.latency_generation()
+        noise = self.noise_generation(signal.signal_power)
+        signal.update_latency(latency)
+        signal.update_noise_power(noise)
+        for i in self.successive.values():
+            i.probe(signal)
+
+    def ase_generation(self):
+        return line5.ase_generation(self)
+
+    def nli_generation(self, P_ch):
+        return line5.nli_generation(self, P_ch)
